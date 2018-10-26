c818de3467e4557e67cd42e9098bed3a4c3a69f1
bhois <bernd.hois@bearingpoint.com>
2018-10-26 22:57:14 +0200

    Update

--- programming-elixir/my_list.exs
+++ programming-elixir/my_list.exs
@@ -118,4 +118,34 @@ defmodule MyList do
   def flatten2([]), do: []
   def flatten2([ head | tail ]), do: flatten2(head) ++ flatten2(tail)
   def flatten2(value), do: [value]
+
+  def primes(a) do
+    for x <- span(2, a), Enum.all?(span(2, x - 1), fn y -> rem(x, y) > 0 end), do: x
+  end
+
+  def tax_rates do
+    [ NC: 0.075, TX: 0.08 ]
+  end
+
+  def orders do
+    [
+      [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+      [ id: 124, ship_to: :OK, net_amount: 35.50 ],
+      [ id: 125, ship_to: :TX, net_amount: 24.00 ],
+      [ id: 126, ship_to: :TX, net_amount: 44.80 ],
+      [ id: 127, ship_to: :NC, net_amount: 25.00 ],
+      [ id: 128, ship_to: :MA, net_amount: 10.00 ],
+      [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+      [ id: 130, ship_to: :NC, net_amount: 50.00 ]
+    ]
+  end
+
+  def apply_taxrate(tax_rates, order) do
+    tax = Keyword.get(tax_rates, order[:ship_to], 0)
+    Keyword.put(order, :total_amount, order[:net_amount] + order[:net_amount] * tax)
+  end
+
+  def calc() do
+    Enum.map(orders(), fn order -> apply_taxrate(tax_rates(), order) end)
+  end
 end

b39aaa3d54f927521d382b4158b1e4d3281b2f69
bhois <bernd.hois@bearingpoint.com>
2018-12-12 09:16:41 +0100

    Update

--- programming-elixir/sequence/lib/stack/server.ex
+++ programming-elixir/sequence/lib/stack/server.ex
@@ -8,4 +8,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, current_stack) do
     {:reply, current_stack |> List.first(), current_stack |> List.delete_at(0)}
   end
+
+  def handle_cast({:push, item}, current_stack) do
+    {:noreply, [item | current_stack ]}
+  end
 end

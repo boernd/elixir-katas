641db084a018a8fefe4d0d3fa07d377060ebe1b2
bhois <bernd.hois@bearingpoint.com>
2018-12-13 21:30:01 +0100

    Update

--- programming-elixir/sequence/lib/stack/server.ex
+++ programming-elixir/sequence/lib/stack/server.ex
@@ -1,6 +1,22 @@
 defmodule Stack.Server do
   use GenServer
 
+  @server Stack.Server
+
+  # API part
+  def start_link(stack) do
+    GenServer.start_link(@server, stack, name: @server)
+  end
+
+  def pop do
+    GenServer.call(@server, :pop)
+  end
+
+  def push(item) do
+    GenServer.cast(@server, {:push, item})
+  end
+
+  # GenServer and Impl part
   def init(stack) do
     {:ok, stack}
   end
@@ -10,6 +26,16 @@ defmodule Stack.Server do
   end
 
   def handle_cast({:push, item}, current_stack) do
+    #raise "oops"
     {:noreply, [item | current_stack ]}
+  end
+
+  def terminate(reason, state) do
+    IO.puts """
+    Terminating
+    ===========
+    Reason: #{inspect reason}
+    State: #{inspect state}
+    """
   end
 end
